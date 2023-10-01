c806cd6a86e1dd2217cc4a1e1eaf657e2c7c6382
darrentan <darren.tan0512@gmail.com>
2023-10-02 01:23:55 +0800

    feat: login for scr

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
+require('dotenv').config();
+
 
 async function parseScr() {
-    // Launch the browser
+
     const browser = await puppeteer.launch({headless: false});
 
     // Open a new tab
@@ -16,10 +18,37 @@ async function parseScr() {
     const confirmButton = await page.$('[class="confirm"]')
     await confirmButton.click();
 
-    
+    await page.waitForSelector('input[name=userName]');
+    await page.type('input[name=userName]', process.env.SCR_USERNAME);
 
-    await page.close();
-    await browser.close(); 
+    await page.waitForSelector('input[name=passWd]');
+    await page.type('input[name=passWd]', process.env.SCR_PASSWORD);
+
+    await page.$eval("button[id='loginButton']", elem => elem.click()); 
+
+    await page.waitForSelector('input[id=txt_Password]');
+    await page.type('input[id=txt_Password]', process.env.SCR_PASSWORD2);
+
+
+    await page.waitForSelector("button[id='Button_OK']");
+    await page.focus("button[id='Button_OK']")
+    await page.click("button[id='Button_OK']");
+   
 }
 
 parseScr();
+
+
+// notes
+
+// // not work
+// await page.waitForSelector(mySelector);
+// await page.click(mySelector);
+
+// // not work 
+// await page.waitForSelector(mySelector);
+// await page.focus(mySelector);
+// await page.click(mySelector);
+
+// // work
+// await page.$eval(mySelector, (elem) => elem.click());
